b36d97252c2d0cbfac482d82d63f6e4be7f18593
Billy Wood <billyc.wood4@gmail.com>
2024-09-13 19:55:17 -0600

    Added descriptions for the videos (not supported for mobile)

--- app/ui/video.js
+++ app/ui/video.js
@@ -8,9 +8,11 @@ export default function Video({title, thumbnail, src, description}) {
     return(
         <div className="mb-10">
             <h1 className="text-xl mb-2">{title}</h1>
-            <div className="rounded-2xl hover:z-0 hover:bg-vvm-pink hover:opacity-60 ease-in-out duration-200 hover:cursor-pointer" onClick={() => setIsVisible('absolute')}>
-                <img className="rounded-2xl aspect-auto" src={thumbnail} />
+            <div className="group rounded-2xl hover:z-0  grid place-items-center [grid-template-areas:'stack']" onClick={() => setIsVisible('absolute')}>
+                <img className="rounded-2xl aspect-auto group-hover:bg-vvm-pink group-hover:opacity-60 ease-in-out duration-200 group-hover:cursor-pointer [grid-area:stack]" src={thumbnail} />
+                <p className="hidden sm:block z-10 opacity-0 group-hover:opacity-100 ease-in-out duration-200 px-6 [grid-area:stack]">{description}</p>
             </div>
+        
             <div className={`${isVisible} w-full h-full absolute left-0 top-0 pt-60 px-4 bg-custom-gray bg-opacity-85`}>
                 <div className="flex flex-col gap-y-6">
                     <div className="hover:cursor-pointer ml-auto float-right" onClick={() => setIsVisible('hidden')}>
