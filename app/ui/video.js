b394d54d10f176a1226a55844b00a6b0fe4877cb
Billy Wood <billyc.wood4@gmail.com>
2024-09-24 10:09:22 -0600

    video pauses on close now

--- app/ui/video.js
+++ app/ui/video.js
@@ -1,9 +1,18 @@
 "use client";
+import { exit } from "process";
 import { useState } from "react";
 
 export default function Video({title, thumbnail, src, description, ifFade}) {
 
     const [isVisible, setIsVisible] = useState('hidden');
+
+
+    const video = document.querySelector('video');
+    
+    function togglePlay() {
+        video.pause();
+        setIsVisible('hidden')
+    }
 
     return(
         <div className="mb-10">
@@ -14,14 +23,14 @@ export default function Video({title, thumbnail, src, description, ifFade}) {
             </div>
         
             <div className={`${isVisible} w-full h-screen left-0 top-14 pt-60 px-4 bg-custom-gray bg-opacity-85 z-10`}>
-                <div className="flex flex-col gap-y-6 w-2/3 mx-auto lg:-translate-y-24">
-                    <div className="hover:cursor-pointer ml-auto float-right" onClick={() => setIsVisible('hidden')}>
+                <div className="flex flex-col gap-y-6 w-2/3 mx-auto md:-translate-y-24">
+                    <div className="hover:cursor-pointer ml-auto float-right" onClick={togglePlay}>
                         <span className="block w-6 border-b-2 rotate-45" />
                         <span className="block w-6 border-b-2 -rotate-45 -translate-y-0.5" />
                     </div>
                     {
                         //<iframe className={`rounded-2xl aspect-video`} src={src} title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen" referrerpolicy="strict-origin-when-cross-origin"></iframe>
-                        <video className="rounded-2xl aspect-video" width="" height="" controls preload="none">
+                        <video id="video" className="rounded-2xl aspect-video" width="" height="" controls preload="none">
                             <source src={src} type="video/mp4" />
                             Your browser does not support the video tag.
                         </video>
