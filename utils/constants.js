67b2af70c0a0cfba92599b3e437d5dd58333b305
Billy Wood <billyc.wood4@gmail.com>
2024-09-24 10:50:07 -0600

    removed description from sushi and real estate for the time being

--- utils/constants.js
+++ utils/constants.js
@@ -33,7 +33,7 @@ export const videos = [
         src: '/videos/Daves Sushi Promo Final (MP4).mp4',
         title: "Dave's Sushi",
         thumbnail: '/thumbnails/DavesSushiThumbnail.jpg',
-        description: "A little bit of honey, a whole lot of jack. A blend of Jack Daniel’s Tennessee Whiskey and a unique honey liqueur combines for a taste that’s one-of-a-kind and unmistakably Jack. With hints of honey and a finish that’s naturally smooth, Jack Daniel’s Tennessee Honey offers a taste of the unexpected.",
+        description: "",
         featured: false,
     },
     {
@@ -41,7 +41,7 @@ export const videos = [
         src: '/videos/3126 Savannah FINAL Directors Cut (MP4).mp4',
         title: "Savage Real Estate Group",
         thumbnail: '/thumbnails/SavageThumbnail.jpg',
-        description: "This cereal is bursting with red berries. People have fallen for Special K® Red Berries Cereal. With rice and wheat flakes and crunchy fruity goodness, what's not to love? It's a bright, flavorful breakfast, and perfect for afternoon and late-night snacks too.",
+        description: "",
         featured: false,
     },
 ];
