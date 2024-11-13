e63cdc483faea2d031c43d5a8c082576d24c7843
Billy Wood <billyc.wood4@gmail.com>
2024-11-12 22:26:39 -0700

    commented out bg images except for contact page

--- app/about/page.js
+++ app/about/page.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function About(){
     return (
         <main className="text-center relative">
-            <Image src={'/VVM-bg-images/adrien-olichon-X8o-P23flgI-unsplash.jpg'} alt="background image" style={{objectFit:'cover'}} className='-z-10' fill priority />
+            {/* <Image src={'/VVM-bg-images/adrien-olichon-X8o-P23flgI-unsplash.jpg'} alt="background image" style={{objectFit:'cover'}} className='-z-10' fill priority /> */}
             <div className="pt-10 pb-10 border-b border-vvm-teal">
                 <h2 className="font-bold text-2xl">Our Purpose</h2>
                 <p className="italic mt-6 mb-10">Bringing Your Vision to Life, Frame by Frame.</p>
--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -9,7 +9,7 @@ export default function Portfolio(){
 
     return (
         <main className="flex flex-col min-h-screen relative items-center text-center py-10 lg:px-24">
-            <Image src={'/VVM-bg-images/adrien-olichon-X8o-P23flgI-unsplash.jpg'} alt="background image" style={{objectFit:'cover'}} className='-z-10' fill priority />
+            {/* <Image src={'/VVM-bg-images/adrien-olichon-X8o-P23flgI-unsplash.jpg'} alt="background image" style={{objectFit:'cover'}} className='-z-10' fill priority /> */}
             <h1 className="font-extrabold text-2xl mb-10">Get a Closer Look into our Vision</h1>
 
             <div className="grid grid-cols-1 px-10 lg:grid-cols-2 gap-x-6 h-full">
