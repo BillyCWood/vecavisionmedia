a41a2d0259d140df6543e8c75ed08f209f4e8148
Billy Wood <billyc.wood4@gmail.com>
2024-11-14 23:51:06 -0700

    minor sizing adjustments to home page photos

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
         <Image src={'/images/robert-shunev-mS1nlYbq1kA-unsplash.jpg'} alt="camera 1" style={{objectFit:"cover"}} quality={75} className="-z-10" fill priority />
         <div className="flex flex-col items-center gap-y-2 pt-32 px-10 md:px-24 lg:px-48">
           <h1 className="font-bold text-2xl text-vvm-lightblue">Bringing your vision to life, one frame at a time</h1>
-          <p className="mb-6">72% of customers say they prefer learning about a product or service through video. We can help you create impressive video content to grow your business and give customers the desire to have your product or service.</p>
+          <p className="mb-6 md:text-center">72% of customers say they prefer learning about a product or service through video. We can help you create impressive video content to grow your business and give customers the desire to have your product or service.</p>
           <Link href="/contact"><Button text="Learn How" /></Link>
         </div>
       </div>
@@ -61,7 +61,7 @@ export default function Home() {
         
         <Image src={'/images/theregisti-HSXIp58yPyI-unsplash.jpg'} alt="camera 1" width={960} height={960} />
         
-        <div className="flex flex-col items-center gap-y-2 pt-32 px-16">
+        <div className="flex flex-col items-center gap-y-2 pt-24 px-16">
           <h1 className="font-bold text-2xl text-vvm-lightblue">Inspire, Engage, and Impress</h1>
           <p className="mb-6 text-center">Hook your audience, engage their senses, and leave an unforgettable impression with captivaing video productions that elevate your brand.</p>
           <Link href="/services"><Button text="View Services" /></Link>
@@ -72,9 +72,9 @@ export default function Home() {
       <div className="hidden min-h-screen max-h-screen lg:max-2xl:flex flex-col justify-evenly bg-[#000]">
 
         <div className="relative mx-auto">
-          <div className="bg-vvm-pink w-full h-[640px] absolute top-0 left-0 -translate-y-8 -translate-x-8"></div>
-          <Image src={'/images/theregisti-HSXIp58yPyI-unsplash.jpg'} alt="camera 2" width={960} height={960} className="relative z-[1] shadow-custom-gray shadow-lg" />
-          <div className="bg-vvm-blue w-full h-[640px] absolute top-0 left-0 translate-y-8 translate-x-8"></div>
+          <div className="bg-vvm-pink w-full h-[480px] absolute top-0 left-0 -translate-y-8 -translate-x-8 shadow-md shadow-vvm-blue"></div>
+          <Image src={'/images/theregisti-HSXIp58yPyI-unsplash.jpg'} alt="camera 2" width={720} height={1} className="relative z-[1] shadow-custom-gray shadow-lg" />
+          <div className="bg-vvm-blue w-full h-[480px] absolute top-0 left-0 translate-y-8 translate-x-8 shadow-md shadow-vvm-pink"></div>
           
         </div>
 
@@ -115,7 +115,7 @@ export default function Home() {
         <Image src={'/images/hunter-moranville-CMEpx6q7xrs-unsplash.jpg'} alt="camera 3" style={{objectFit:"cover"}} quality={75} className="-z-10" fill />
         <div className="flex flex-col items-center gap-y-2 pt-16 px-6">
           <h1 className="font-bold text-2xl text-vvm-lightblue">See it for Yourself</h1>
-          <p className="mb-6">Examples of how we've brought our clients vision to life.</p>
+          <p className="mb-6 rounded-full bg-custom-gray/50">Examples of how we've brought our clients vision to life.</p>
           <Link href="/portfolio"><Button text="View the Vision" /></Link>
         </div>
 
