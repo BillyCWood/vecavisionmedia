7612222376704a822b5e42e4d6bc6c8257d79dcc
Billy Wood <billyc.wood4@gmail.com>
2024-12-02 14:35:36 -0700

    send emails to Jaden's email

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -12,7 +12,7 @@ export async function POST(request:Request, res:Response) {
     const { fname, lname, email, phone, org, message } = await request.json();
     const { data, error } = await resend.emails.send({
       from: 'Veca Vision Media <noreply@resend.dev>',
-      to: 'billyc.wood4@gmail.com',
+      to: 'veca.vision@gmail.com',
       subject: 'New Contact Request from ' + fname + ' ' + lname + '!',
       react: EmailTemplate({ fname, lname, email, phone, org, message }),
     });
