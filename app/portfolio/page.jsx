93bad812ea1bb9a75ea362c1b38bb7a0a4a15b20
Billy Wood <billyc.wood4@gmail.com>
2025-05-02 20:51:32 -0600

    moved tailwind styles to css file

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -10,7 +10,7 @@ export default function Portfolio() {
   const [show, setShow] = useState(false);
 
   return (
-    <main className="bg-black min-h-screen">
+    <main className="min-h-screen">
         <div className="hidden lg:flex mt-6 ml-10">
             <button className="bg-white border-[2px] border-white text-black hover:bg-[#aaa] hover:text-white transition-colors duration-300 ease-linear p-2 w-40 rounded-3xl z-[1]" onClick={()=> {setShow(!show); console.log(show)}}>Search by topic</button>
             {
@@ -18,11 +18,11 @@ export default function Portfolio() {
                 // because Tailwind does not support dynamic classes... SO BACK TO THE ORIGINAL WAY; IF IT AINT BROKE DON'T FIX IT!
             }
             <div className={`z-0 flex gap-x-4 ml-6`}>
-                <button className={`bg-[#4d0066] p-2 w-32 rounded-3xl border-[2px] border-[#4d0066] hover:bg-white hover:text-[#4d0066] transition-all duration-300 ease-linear  ${show ?  'translate-x-0' : '-translate-x-[152px]'}`} onClick={() => {setCategory('product'); setShow(!show)}}>Product</button>
-                <button className={`bg-[#01417c] p-2 w-32 rounded-3xl border-[2px] border-[#01417c] hover:bg-white hover:text-[#01417c] transition-all duration-300 ease-linear  ${show ?  'translate-x-0' : '-translate-x-[300px]'}`} onClick={() => {setCategory('service'); setShow(!show)}}>Service/Brand</button>
-                <button className={`bg-[#017c64] p-2 w-32 rounded-3xl border-[2px] border-[#017c64] hover:bg-white hover:text-[#017c64] transition-all duration-300 ease-linear  ${show ?  'translate-x-0' : '-translate-x-[450px]'}`} onClick={() => {setCategory('real estate'); setShow(!show)}}>Real Estate</button>
-                <button className={`bg-[#1b5c78] p-2 w-32 rounded-3xl border-[2px] border-[#1b5c78] hover:bg-white hover:text-[#1b5c78] transition-all duration-300 ease-linear  ${show ?  'translate-x-0' : '-translate-x-[600px]'}`} onClick={() => {setCategory('industrial'); setShow(!show)}}>Industrial</button>
-                <button className={`bg-white border-[2px] border-white text-black hover:bg-[#aaa] hover:text-white transition-all duration-300 ease-linear p-2 w-32 rounded-3xl ${show ?  'translate-x-0' : '-translate-x-[750px]'}`} onClick={() => {setCategory(''); setShow(!show)}}>Clear</button>
+                <button className={`portfolio-filter-button bg-[#4d0066] border-[#4d0066] hover:text-[#4d0066] ${show ?  'translate-x-0' : '-translate-x-[152px]'}`} onClick={() => {setCategory('product'); setShow(!show)}}>Product</button>
+                <button className={`portfolio-filter-button bg-[#01417c] border-[#01417c] hover:text-[#01417c] ${show ?  'translate-x-0' : '-translate-x-[300px]'}`} onClick={() => {setCategory('service'); setShow(!show)}}>Service/Brand</button>
+                <button className={`portfolio-filter-button bg-[#017c64] border-[#017c64] hover:text-[#017c64] ${show ?  'translate-x-0' : '-translate-x-[450px]'}`} onClick={() => {setCategory('real estate'); setShow(!show)}}>Real Estate</button>
+                <button className={`portfolio-filter-button bg-[#1b5c78] border-[#1b5c78] hover:text-[#1b5c78] ${show ?  'translate-x-0' : '-translate-x-[600px]'}`} onClick={() => {setCategory('industrial'); setShow(!show)}}>Industrial</button>
+                <button className={`portfolio-filter-button bg-white border-white text-black hover:bg-[#aaa] hover:text-white ${show ?  'translate-x-0' : '-translate-x-[750px]'}`} onClick={() => {setCategory(''); setShow(!show)}}>Clear</button>
             </div>
         </div>
 
@@ -30,11 +30,11 @@ export default function Portfolio() {
             <button className={`group py-4 bg-white border-[2px] border-white text-black hover:bg-[#aaa] hover:text-white w-56 ${show ? 'rounded-t-3xl' : 'rounded-3xl'} transition-all duration-300 ease-linear flex items-center justify-center z-[1]`} onClick={()=> {setShow(!show)}}>Search by topic <span className="block border-b-2 group-hover:border-b-white transition-colors duration-500 border-b-black w-2 rotate-45 translate-y-[1px] translate-x-[3px]" /><span className="block border-b-2 group-hover:border-b-white transition-colors duration-500 border-b-black w-2 -rotate-45 translate-y-[1px]" /></button>
             <div className={`w-56 mx-auto z-0 ${show ? '' : 'h-0'}`}>
 
-                <button className={`py-4 bg-[#4d0066] w-56 border-[2px] border-[#4d0066] hover:bg-white hover:text-[#4d0066] transition-all duration-300 ease-linear ${show ?  'translate-y-0' : 'opacity-0 -translate-y-[60px] rounded-3xl'}`} onClick={() => {setCategory('product'); setShow(!show)}}>Product</button>
-                <button className={`py-4 bg-[#01417c] w-56 border-[2px] border-[#01417c] hover:bg-white hover:text-[#01417c] transition-all duration-300 ease-linear ${show ?  'translate-y-0' : '-translate-y-[120px] rounded-3xl'}`} onClick={() => {setCategory('service'); setShow(!show)}}>Service/Brand</button>
-                <button className={`py-4 bg-[#017c64] w-56 border-[2px] border-[#017c64] hover:bg-white hover:text-[#017c64] transition-all duration-300 ease-linear delay-[25ms] ${show ?  'translate-y-0' : '-translate-y-[180px] rounded-3xl'}`} onClick={() => {setCategory('real estate'); setShow(!show)}}>Real Estate</button>
-                <button className={`py-4 bg-[#1b5c78] w-56 border-[2px] border-[#1b5c78] hover:bg-white hover:text-[#1b5c78] transition-all duration-300 ease-linear delay-[25ms] ${show ?  'translate-y-0' : '-translate-y-[240px] rounded-3xl'}`} onClick={() => {setCategory('industrial'); setShow(!show)}}>Industrial</button>
-                <button className={`py-4 bg-white border-[2px] border-white text-black hover:bg-[#aaa] hover:text-white transition-all duration-300 ease-linear w-56 rounded-b-3xl ${show ?  'translate-y-0' : '-translate-y-[300px] rounded-3xl'}`} onClick={() => {setCategory(''); setShow(!show)}}>Clear</button>
+                <button className={`portfolio-filter-button-responsive bg-[#4d0066] border-[#4d0066] hover:text-[#4d0066] ${show ?  'translate-y-0' : 'opacity-0 -translate-y-[60px] rounded-3xl'}`} onClick={() => {setCategory('product'); setShow(!show)}}>Product</button>
+                <button className={`portfolio-filter-button-responsive bg-[#01417c] border-[#01417c] hover:text-[#01417c] ${show ?  'translate-y-0' : '-translate-y-[120px] rounded-3xl'}`} onClick={() => {setCategory('service'); setShow(!show)}}>Service/Brand</button>
+                <button className={`portfolio-filter-button-responsive bg-[#017c64] border-[#017c64] hover:text-[#017c64] delay-[25ms] ${show ?  'translate-y-0' : '-translate-y-[180px] rounded-3xl'}`} onClick={() => {setCategory('real estate'); setShow(!show)}}>Real Estate</button>
+                <button className={`portfolio-filter-button-responsive bg-[#1b5c78] border-[#1b5c78] hover:text-[#1b5c78] delay-[25ms] ${show ?  'translate-y-0' : '-translate-y-[240px] rounded-3xl'}`} onClick={() => {setCategory('industrial'); setShow(!show)}}>Industrial</button>
+                <button className={`portfolio-filter-button-responsive bg-white border-white text-black hover:bg-[#aaa] hover:text-white rounded-b-3xl ${show ?  'translate-y-0' : '-translate-y-[300px] rounded-3xl'}`} onClick={() => {setCategory(''); setShow(!show)}}>Clear</button>
             </div>
         </div>
 
